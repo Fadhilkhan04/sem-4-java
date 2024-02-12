51e38f44205714876e9223295b0c3680fc797710
Fadhilkhan04 <114307570+Fadhilkhan04@users.noreply.github.com>
2024-02-12 23:27:46 +0530

    pgm 2

---
+++ 2/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String a;
+    Scanner scan = new Scanner(System.in);
+    System.out.println("enter the data:");
+    a = scan.nextLine();
+    System.out.println("you entered :" + a);
+  }
+}
