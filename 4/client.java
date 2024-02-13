fe7978f32e95923cb7ef36f109ef35709d88ae7c
Fadhil <114307570+Fadhilkhan04@users.noreply.github.com>
2024-02-13 10:16:04 +0530

    Add files via upload

---
+++ 4/client.java
@@ -0,0 +1,20 @@
+import java.io.*;  
+import java.net.*;
+import java.util.*;
+
+public class client{
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+        try {
+            Socket s = new Socket("localhost",6666);
+            DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+            dout.writeUTF(a.nextLine());  
+            dout.flush();  
+            dout.close();  
+            s.close(); 
+            a.close(); 
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
