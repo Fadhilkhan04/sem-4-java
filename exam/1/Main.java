2c8a8047d590b703180af465b3c6e483dcd8fab6
Fadhilkhan04 <114307570+Fadhilkhan04@users.noreply.github.com>
2024-05-17 10:30:49 +0530

    ping request

---
+++ exam/1/Main.java
@@ -0,0 +1,20 @@
+import java.net.*;
+import java.io.*;
+
+public class Main {
+
+  public static void sendPingRequest(String ipAddress) throws UnknownHostException, IOException {
+    InetAddress ip = InetAddress.getByName(ipAddress);
+    System.out.println("sending ping request:" + ipAddress);
+    if (ip.isReachable(5000))
+      System.out.println("host is reachable");
+    else
+      System.out.println("host is not reachable");
+  }
+
+  public static void main(String[] args) throws UnknownHostException, IOException {
+
+    String ipAddress = "127.0.0.1";
+    sendPingRequest(ipAddress);
+  }
+}
