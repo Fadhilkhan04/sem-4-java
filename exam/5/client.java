8e0fc8d1807d2d4e86e435ece67efca27ea6a195
Fadhilkhan04 <114307570+Fadhilkhan04@users.noreply.github.com>
2024-05-18 02:27:59 +0530

    file transfer

--- exam/5/client.java
+++ exam/5/client.java
@@ -2,18 +2,18 @@ import java.net.*;
 import java.io.*;
 
 public class client {
-  private static DataInputStream din = null;
+  // private static DataInputStream din = null;
   private static DataOutputStream dout = null;
 
   public static void main(String[] args) {
     try {
       Socket s = new Socket("localhost", 6666);
-      din = new DataInputStream(s.getInputStream());
+      // din = new DataInputStream(s.getInputStream());
       dout = new DataOutputStream(s.getOutputStream());
       System.out.println("sending file to server");
       sendFile("./test.txt");
       s.close();
-      din.close();
+      // din.close();
 
     } catch (Exception e) {
       // TODO: handle exception
@@ -25,12 +25,15 @@ public class client {
     int bytes = 0;
     File f = new File("./test.txt");
     FileInputStream fis = new FileInputStream(f);
-    dout.writeLong(f.length());
+    int size = (int) f.length();
+    dout.writeLong(size);
     byte[] buffer = new byte[4 * 1024];
 
     do {
       bytes = fis.read(buffer);
+      // bytes = fis.read(buffer, 0, (int) Math.min(buffer.length, sizeLeft));
       dout.write(buffer, 0, bytes);
+      // size -= bytes;
       dout.flush();
     } while (bytes != -1);
     fis.close();
--- exam/5/server.java
+++ exam/5/server.java
@@ -4,14 +4,14 @@ import java.net.*;
 public class server {
 
   private static DataInputStream din = null;
-  private static DataOutputStream dout = null;
+  // private static DataOutputStream dout = null;
 
   public static void main(String[] args) {
     try {
       ServerSocket ss = new ServerSocket(6666);
       Socket s = ss.accept();
       din = new DataInputStream(s.getInputStream());
-      dout = new DataOutputStream(s.getOutputStream());
+      // dout = new DataOutputStream(s.getOutputStream());
       receiveFile("./receive.txt");
 
     } catch (Exception e) {
@@ -26,9 +26,10 @@ public class server {
     byte[] buffer = new byte[4 * 1024];
 
     do {
-      bytes = din.read(buffer, 0, (int) Math.min(buffer.length, size));
+      bytes = din.read(buffer);
+      // bytes = din.read(buffer, 0, (int) Math.min(buffer.length, size));
       fis.write(buffer, 0, bytes);
-      size -= bytes;
+      // size -= bytes;
     } while (size > 0 && bytes != -1);
     // read file size
     System.out.println("file is received");
